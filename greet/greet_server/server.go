ca2c1018849ee5aaea1b349c46aff941f409f916
Blaine Richardson <blainerrichardson@gmail.com>
2021-06-13 18:48:26 -0400

    feat: section-7: finishing all lectures for section-7

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"grpc-golang-master-class-build-modern-api-and-microservices/greet/greetpb"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -18,17 +19,18 @@ type server struct {
 }
 
 func (s *server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet function was invoked with %v", req)
+	fmt.Printf("Greet function was invoked with %v\n", req)
 	firstName := strings.TrimSpace(req.GetGreeting().GetFirstName())
 	lastName := strings.TrimSpace(req.GetGreeting().GetLastName())
 	res := &greetpb.GreetResponse{
 		Result: "Hello " + firstName + " " + lastName,
 	}
+	fmt.Println("Greet function was successful")
 	return res, nil
 }
 
 func (s *server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
-	fmt.Printf("GreetManyTimes function was invoked with %v", req)
+	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
 	firstName := strings.TrimSpace(req.GetGreeting().GetFirstName())
 	lastName := strings.TrimSpace(req.GetGreeting().GetLastName())
 	for i := 0; i < 10; i++ {
@@ -36,9 +38,45 @@ func (s *server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greet
 			Result: "Hello " + firstName + " " + lastName + " number " + strconv.Itoa(i),
 		}
 		stream.Send(res)
-		time.Sleep(1000 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
+	fmt.Println("GreetManyTimes function was successful")
 	return nil
+}
+
+func (s *server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+	fmt.Println("LongGreet function was invoked")
+
+	result := ""
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// done reading stream
+			er := stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result: result,
+			})
+			if er != nil {
+				log.Fatalf("Error while sending and closing client stream: %v\n", er)
+				return err
+			}
+			fmt.Println("LongGreet function was successful")
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+			return err
+		}
+
+		firstName := strings.TrimSpace(req.GetGreeting().GetFirstName())
+		lastName := strings.TrimSpace(req.GetGreeting().GetLastName())
+
+		greeting := "Hello " + firstName + " " + lastName + "! "
+
+		fmt.Printf("LongGreet function saying hi to %v\n", greeting)
+
+		result += greeting
+	}
 }
 
 func main() {
